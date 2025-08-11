d33b42952b0c9acc20d3b78282fd9b6ad72e81a6
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2025-08-11 23:32:49 +0900

    Added new project

---
+++ tomcat/simple-json-api/src/main/java/com/TsutomuNakamura/learn_java/tomcat/simple_json_api/HelloApi.java
@@ -0,0 +1,35 @@
+package com.TsutomuNakamura.learn_java.tomcat.simple_servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hello")
+public class HelloApi extends HttpServlet {
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        
+        response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+        
+        PrintWriter out = response.getWriter();
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Hello Servlet</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>Hello from Servlet!</h1>");
+        out.println("<p>This is a simple servlet running on Tomcat 10+</p>");
+        out.println("<p>Current time: " + new java.util.Date() + "</p>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
