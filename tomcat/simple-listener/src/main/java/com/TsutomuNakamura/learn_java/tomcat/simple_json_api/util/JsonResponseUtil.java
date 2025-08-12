1a8ce89b661fe38fc886f1539396db40e647ab9f
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2025-08-13 00:19:46 +0900

    Updated

--- tomcat/simple-listener/src/main/java/com/TsutomuNakamura/learn_java/tomcat/simple_json_api/util/JsonResponseUtil.java
+++ tomcat/simple-listener/src/main/java/com/TsutomuNakamura/learn_java/tomcat/simple_json_api/util/JsonResponseUtil.java
@@ -24,10 +24,4 @@ public class JsonResponseUtil {
         out.print(jsonString);
         out.flush();
     }
-    
-    public static void setCorsHeaders(HttpServletResponse response) {
-        response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-        response.setHeader("Access-Control-Allow-Headers", "Content-Type");
-    }
 }
