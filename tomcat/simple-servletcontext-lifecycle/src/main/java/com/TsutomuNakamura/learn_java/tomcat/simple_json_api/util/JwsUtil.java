57f294ace32d52120925d6efcabc8ad8b84fa10c
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2025-08-13 21:52:31 +0900

    Updated

--- tomcat/simple-servletcontext-lifecycle/src/main/java/com/TsutomuNakamura/learn_java/tomcat/simple_json_api/util/JwsUtil.java
+++ tomcat/simple-servletcontext-lifecycle/src/main/java/com/TsutomuNakamura/learn_java/tomcat/simple_json_api/util/JwsUtil.java
@@ -5,11 +5,7 @@ import com.nimbusds.jose.crypto.ECDSASigner;
 import com.nimbusds.jwt.JWTClaimsSet;
 import com.nimbusds.jwt.SignedJWT;
 
-import java.security.KeyFactory;
-import java.security.PrivateKey;
 import java.security.interfaces.ECPrivateKey;
-import java.security.spec.PKCS8EncodedKeySpec;
-import java.util.Base64;
 import java.util.Date;
 
 /**
@@ -20,7 +16,7 @@ public class JwsUtil {
     private final ECPrivateKey privateKey;
     
     public JwsUtil() throws Exception {
-        this.privateKey = loadPrivateKey();
+        this.privateKey = KeyUtil.loadEcPrivateKey();
     }
     
     /**
@@ -95,39 +91,6 @@ public class JwsUtil {
         
         // Return serialized JWS
         return signedJWT.serialize();
-    }
-    
-    /**
-     * Loads the private key from the configuration file
-     * 
-     * @return ECPrivateKey for signing
-     * @throws Exception if key loading fails
-     */
-    private ECPrivateKey loadPrivateKey() throws Exception {
-        // Load private key from configuration
-        String privateKeyPEM = ConfigUtil.getJwsPrivateKey();
-        
-        // Remove PEM headers and decode Base64
-        String cleanedKey = privateKeyPEM
-                .replace("-----BEGIN PRIVATE KEY-----", "")
-                .replace("-----END PRIVATE KEY-----", "")
-                .replaceAll("\\s", "");
-        
-        byte[] keyBytes = Base64.getDecoder().decode(cleanedKey);
-        
-        // Create key specification
-        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(keyBytes);
-        
-        // Generate private key
-        KeyFactory keyFactory = KeyFactory.getInstance("EC");
-        PrivateKey privateKey = keyFactory.generatePrivate(keySpec);
-        
-        if (!(privateKey instanceof ECPrivateKey)) {
-            throw new IllegalArgumentException("Private key is not an EC private key");
-        }
-        
-        System.out.println("Successfully loaded JWS private key from configuration");
-        return (ECPrivateKey) privateKey;
     }
     
     /**
